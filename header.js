904393b1eb3a329d7e42eeb1a5dd27d29183648d
EmersonJr <contato.juniormorales@gmail.com>
2024-05-27 16:34:51 -0300

    Terceiro commit

--- header.js
+++ header.js
@@ -1,42 +1,58 @@
 const main = document.querySelector(".container");
- 
 const urlImg = "http://10.92.198.38:8080/";
- 
-async function getPersons() {
-  // Comando Fetch usando async await
-  const response = await fetch("http://10.92.198.38:8080/feed/posts");
- 
-  // Trasfromando JSON em um Objeto Utilizavél
+let currentPage = 1;
+const postsPerPage = 3;
+
+async function getPersons(page = 1) {
+  const response = await fetch(`http://10.92.198.38:8080/feed/posts?page=${page}&limit=${postsPerPage}`);
   const posts = await response.json();
- 
   console.log(posts);
+  return posts;
 }
- 
-fetch("http://10.92.198.38:8080/feed/posts")
-  .then((rest) => rest.json())
-  .then((data) => cards(data));
- 
+
 function cards(data) {
-  console.log(data);
- 
+  main.innerHTML = ""; // Limpar posts antigos
   const arrayDatas = data.posts;
- 
+  if (arrayDatas.length === 0) {
+    const message = document.createElement("p");
+    message.className = "no-images-message";
+    message.textContent = "Não há mais imagens para exibir.";
+    main.appendChild(message);
+    return;
+  }
   arrayDatas.forEach((element) => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
- 
-    <div class="banner"><img
-    src="${urlImg + element.imageUrl}"
-  /></div>
-    <div class="content">
-      <div class="texts">
-        <h3 class="name">${element.title}</h3>
-        <h5 class="species">${element.content}</h5>
- 
+      <div class="banner"><img src="${urlImg + element.imageUrl}" /></div>
+      <div class="content">
+        <div class="texts">
+          <h3 class="name">${element.title}</h3>
+          <h5 class="species">${element.content}</h5>
+        </div>
       </div>
-    </div>
-  `;
+    `;
     main.appendChild(card);
   });
 }
+
+
+async function loadPage(page) {
+  const data = await getPersons(page);
+  cards(data);
+}
+
+document.getElementById("nextPage").addEventListener("click", () => {
+  currentPage++;
+  loadPage(currentPage);
+});
+
+document.getElementById("prevPage").addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    loadPage(currentPage);
+  }
+});
+
+// Carregar a primeira página ao iniciar
+loadPage(currentPage);
